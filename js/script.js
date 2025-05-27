c0db537c8a1e51aec2bb547901d2e722a146383d
Eduardo Liborio <edupedro.eng@gmail.com>
2025-05-26 21:33:03 -0300

    Portfolio

---
+++ js/script.js
@@ -0,0 +1,86 @@
+const navLinks = document.querySelectorAll('header nav a');
+const logoLink = document.querySelector('.logo');
+const sections = document.querySelectorAll('section');
+
+const activePage = () => {
+    navLinks.forEach(link => {
+        link.classList.remove('active');
+    });
+
+    sections.forEach(section => {
+        section.classList.remove('active');
+    });
+}
+
+navLinks.forEach((link, idx) => {
+    link.addEventListener('click', () => {
+        if (!link.classList.contains('active')) {
+            activePage();
+
+            link.classList.add('active');
+
+            setTimeout(() =>{
+                sections[idx].classList.add('active');
+            }, 1100);
+        }
+    })
+}) 
+
+logoLink.addEventListener('click', () => {
+    if (!navLinks[0].classList.contains('active')) {
+        activePage();
+
+        navLinks[0].classList.add('active');
+
+        setTimeout(() =>{
+                sections[0].classList.add('active');
+            }, 1100);
+    }
+})
+
+
+
+
+
+const arrowRight = document.querySelector('.portfolio-box .navigation .arrow-right');
+const arrowLeft = document.querySelector('.portfolio-box .navigation .arrow-left');
+
+let index = 0;
+
+const activePortfolio = () => {
+    const imgSlide = document.querySelector('.portfolio-carousel .img-slide');
+    const portfolioDetails = document.querySelectorAll('.portfolio-detail')
+
+    imgSlide.style.transform = `translateX(calc(${index * -100}% - ${index * 2}rem))`;
+
+    portfolioDetails.forEach(detail => {
+        detail.classList.remove('active');
+    });
+    portfolioDetails[index].classList.add('active');
+}
+
+arrowRight.addEventListener('click', () => {
+    if (index < 1) {
+        index++;
+        arrowLeft.classList.remove('disabled')
+    }
+    else {
+        index = 2;
+        arrowRight.classList.add('disabled')
+    }
+
+    activePortfolio();
+});
+
+arrowLeft.addEventListener('click', () => {
+    if (index > 1) {
+        index--;
+        arrowRight.classList.remove('disbled')
+    }
+    else {
+        index = 0;
+        arrowLeft.classList.add('disabled')
+    }
+
+    activePortfolio();
+});
